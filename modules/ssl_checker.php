3998d48502643c9a8645f252601bc3843078cf6b
Peju3ncer <agusornanda@gmail.com>
2025-08-16 03:04:20 +0800

    Update ssl_checker.php

--- modules/ssl_checker.php
+++ modules/ssl_checker.php
@@ -1,7 +1,7 @@
 <?php
 function checkSSL($target) {
     echo Colors::YELLOW."[🔎] Checking SSL certificate for $target\n".Colors::RESET;
-    
+
     $context = stream_context_create(["ssl" => ["capture_peer_cert" => true]]);
     $read = @stream_socket_client("ssl://".$target.":443", $errno, $errstr, 5, STREAM_CLIENT_CONNECT, $context);
 
@@ -11,3 +11,21 @@ function checkSSL($target) {
     }
 
     $params = stream_context_get_params($read);
+    if (isset($params['options']['ssl']['peer_certificate'])) {
+        $certInfo = openssl_x509_parse($params['options']['ssl']['peer_certificate']);
+
+        if ($certInfo) {
+            echo Colors::GREEN."[+] SSL Certificate Information:\n".Colors::RESET;
+            echo "    Issuer: " . ($certInfo['issuer']['CN'] ?? 'Unknown') . "\n";
+            echo "    Subject: " . ($certInfo['subject']['CN'] ?? 'Unknown') . "\n";
+            echo "    Valid From: " . date('Y-m-d H:i:s', $certInfo['validFrom_time_t']) . "\n";
+            echo "    Valid To: " . date('Y-m-d H:i:s', $certInfo['validTo_time_t']) . "\n";
+        } else {
+            echo Colors::RED."[-] Failed to parse SSL certificate.\n".Colors::RESET;
+        }
+    } else {
+        echo Colors::RED."[-] No SSL certificate found.\n".Colors::RESET;
+    }
+
+    fclose($read);
+}
